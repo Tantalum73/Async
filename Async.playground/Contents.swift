c80d79bc44d4d0c51e982f12f46a5ebef7b34720
Andreas <developer@anerma.de>
2016-05-13 14:18:47 +0200

    Updated Playground

--- Async.playground/Contents.swift
+++ Async.playground/Contents.swift
@@ -24,26 +24,26 @@ Async.async(QOS: .Main, block: {
     print("Finish task 3")
 }
 
-//Async.main(block: {
-//    print("starting Task 1")
-//    let timeInterval = Double(arc4random_uniform(100)) * 0.01
-//    print("Spleeping for \(timeInterval)")
-//    NSThread.sleepForTimeInterval(1)
-//    print("Finish task 1")
-//    
-//}).main(after: 3, block: {
-//    print("starting Task 2")
-//    let timeInterval = Double(arc4random_uniform(100)) * 0.01
-//    print("Spleeping for \(timeInterval)")
-//    NSThread.sleepForTimeInterval(1)
-//    print("Finish task 2")
-//}).main(after: 5, block: {
-//    print("starting Task 3")
-//    let timeInterval = Double(arc4random_uniform(100)) * 0.01
-//    print("Spleeping for \(timeInterval)")
-//    NSThread.sleepForTimeInterval(1)
-//    print("Finish task 3")
-//})
+Async.main(block: {
+    print("starting Task 1")
+    let timeInterval = Double(arc4random_uniform(100)) * 0.01
+    print("Spleeping for \(timeInterval)")
+    NSThread.sleepForTimeInterval(1)
+    print("Finish task 1")
+    
+}).main(after: 3, block: {
+    print("starting Task 2")
+    let timeInterval = Double(arc4random_uniform(100)) * 0.01
+    print("Spleeping for \(timeInterval)")
+    NSThread.sleepForTimeInterval(1)
+    print("Finish task 2")
+}).main(after: 5, block: {
+    print("starting Task 3")
+    let timeInterval = Double(arc4random_uniform(100)) * 0.01
+    print("Spleeping for \(timeInterval)")
+    NSThread.sleepForTimeInterval(1)
+    print("Finish task 3")
+})
 
 print("Done")
 
