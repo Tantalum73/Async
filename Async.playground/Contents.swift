15c7f09187c03284fe551e7ee77fc8629dc5a2e8
Andreas <developer@anerma.de>
2016-05-15 18:27:14 +0200

    Updated Playground Example Usage

--- Async.playground/Contents.swift
+++ Async.playground/Contents.swift
@@ -11,38 +11,38 @@ XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
 //Use this style:
 Async.async(QOS: .Main, block: {
     print("starting Task 1")
-    let timeInterval = Double(arc4random_uniform(100)) * 0.01
-    print("Spleeping for \(timeInterval)")
-    NSThread.sleepForTimeInterval(1)
+    let timeToSleep = 1.0
+    print("Spleeping for \(timeToSleep)")
+    NSThread.sleepForTimeInterval(timeToSleep)
     print("Finish task 1")
     
 }).then(after: 3, QOS: .Background) { 
     print("starting Task 2")
-    let timeInterval = Double(arc4random_uniform(100)) * 0.01
-    print("Spleeping for \(timeInterval)")
-    NSThread.sleepForTimeInterval(1)
+    let timeToSleep = 2.0
+    print("Spleeping for \(timeToSleep)")
+    NSThread.sleepForTimeInterval(timeToSleep)
     print("Finish task 3")
 }
 
 //Or you can use this syntax:
 Async.main(block: {
     print("starting Task 1")
-    let timeInterval = Double(arc4random_uniform(100)) * 0.01
-    print("Spleeping for \(timeInterval)")
-    NSThread.sleepForTimeInterval(1)
+    let timeToSleep = 1.5
+    print("Spleeping for \(timeToSleep)")
+    NSThread.sleepForTimeInterval(timeToSleep)
     print("Finish task 1")
     
 }).main(after: 3, block: {
     print("starting Task 2")
-    let timeInterval = Double(arc4random_uniform(100)) * 0.01
-    print("Spleeping for \(timeInterval)")
-    NSThread.sleepForTimeInterval(1)
+    let timeToSleep = 2.1
+    print("Spleeping for \(timeToSleep)")
+    NSThread.sleepForTimeInterval(timeToSleep)
     print("Finish task 2")
 }).main(after: 5, block: {
     print("starting Task 3")
-    let timeInterval = Double(arc4random_uniform(100)) * 0.01
-    print("Spleeping for \(timeInterval)")
-    NSThread.sleepForTimeInterval(1)
+    let timeToSleep = 3.3
+    print("Spleeping for \(timeToSleep)")
+    NSThread.sleepForTimeInterval(timeToSleep)
     print("Finish task 3")
 })
 
